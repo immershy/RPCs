8ce1c4a80fb3b97fb7e21dc709c43df13b05a135
zhengwei <zhengwei@u51.com>
2018-04-15 20:50:40 +0800

    init

---
+++ src/main/java/zw/me/rpc/RpcsApplication.java
@@ -0,0 +1,12 @@
+package zw.me.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcsApplication.class, args);
+	}
+}
---
+++ src/test/java/zw/me/rpc/RpcsApplicationTests.java
@@ -0,0 +1,16 @@
+package zw.me.rpc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RpcsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
